a411780983f8f06f6086ad88db54effb46a56e71
Idiat Ameen <idi@Idiats-Air.lan>
2022-02-08 12:35:45 -0500

    resume

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import FileViewer from "react-file-viewer";
 // pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const file = "https://docdro.id/Mf6zXED";
+const file = "https://docdro.id/Tr9Wr3l";
 const type = "pdf";
 export default class Resume extends Component {
   render() {
